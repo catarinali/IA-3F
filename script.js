89a612d8259a2e8d3717fb87466ed75dbf79351e
catarinali <114243672+catarinali@users.noreply.github.com>
2024-08-01 16:47:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+
+const perguntas = [//serve para abrir lista de perguntas
+{//abre o objeto das perguntas
+    enunciado: "Pergunta 1",
+    alternativas:[
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+}
+]
