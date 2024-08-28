311d31acc46337dd6aa128419bb8e42c88085cf1
catarinali <114243672+catarinali@users.noreply.github.com>
2024-08-28 18:29:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,23 +5,29 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 
 const perguntas = [//serve para abrir lista de perguntas
 {//abre o objeto das perguntas
-    enunciado: "Pergunta 1",
-    alternativas:["Alternativa 1","Alternativa 2"]
+    enunciado: "Você usa ou já usou algum tipo de Inteligência Artificial?",
+    alternativas:["Sim","Não"]
 },
 {//abre o objeto das perguntas
-    enunciado: "Pergunta 2",
-    alternativas:["Alternativa 1","Alternativa 2"]
+    enunciado: "Você acha que ela causará algum impacto efetivo no futuro?",
+    alternativas:["Sim","Não"]
 }
 {//abre o objeto das perguntas
-    enunciado: "Pergunta 3",
-    alternativas:["Alternativa 1","Alternativa 2"]
+    enunciado: "Você dixaria uma Inteligência Artificial decidir questões muito bem respondidas, com base científica e social, por você?",
+    alternativas:["Sim","Não"]
 }
 ]
 
-let atual = 0;
+let posicao = 0;
 let perguntaAtual;
 
 function mostraPergunta(){
-    perguntaAtual = perguntas [atual];
-    caixaPergunta.textContent = perguntaAtual
+    perguntaAtual = perguntas [posicao];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlterntivas();
 }
+function mostraAlterntivas(){
+
+}
+
+mostraPergunta();
