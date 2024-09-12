8682c0a6c9126f967bfb4c60e831a65b12dd6e71
catarinali <114243672+catarinali@users.noreply.github.com>
2024-09-12 16:43:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,14 +6,26 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 const perguntas = [//serve para abrir lista de perguntas
 {//abre o objeto das perguntas
     enunciado: "Você usa ou já usou algum tipo de Inteligência Artificial?",
+        alternativas:["Sim","Não"]
+},
+    {//abre o objeto das perguntas
+        enunciado: "Você acha que ela é, no minímo, relativamente boa?",
+        alternativas:["Sim","Não"]
+    },
+{//abre o objeto das perguntas
+    enunciado: "Você acha que que estamos ficando mais burros e dependentes da tecnologia por influência dela?",
     alternativas:["Sim","Não"]
 },
 {//abre o objeto das perguntas
     enunciado: "Você acha que ela causará algum impacto efetivo no futuro?",
     alternativas:["Sim","Não"]
-}
+},
 {//abre o objeto das perguntas
     enunciado: "Você dixaria uma Inteligência Artificial decidir questões muito bem respondidas, com base científica e social, por você?",
+    alternativas:["Sim","Não"]
+},
+{//abre o objeto das perguntas
+    enunciado: "Você acha que ela vai dominar o mundo?",
     alternativas:["Sim","Não"]
 }
 ]
