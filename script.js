dd89211fe90de7f888b23daf98578cd460f306fc
catarinali <114243672+catarinali@users.noreply.github.com>
2024-09-25 18:25:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,37 +4,67 @@ const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
 
 const perguntas = [//serve para abrir lista de perguntas
-{//abre o objeto das perguntas
-    enunciado: "Você usa ou já usou algum tipo de Inteligência Artificial?",
-        alternativas:["Sim","Não"]
-},
     {//abre o objeto das perguntas
-        enunciado: "Você acha que ela é, no minímo, relativamente boa?",
-        alternativas:["Sim","Não"]
-    },
-{//abre o objeto das perguntas
-    enunciado: "Você acha que que estamos ficando mais burros e dependentes da tecnologia por influência dela?",
-    alternativas:["Sim","Não"]
-},
-{//abre o objeto das perguntas
-    enunciado: "Você acha que ela causará algum impacto efetivo no futuro?",
-    alternativas:["Sim","Não"]
-},
-{//abre o objeto das perguntas
-    enunciado: "Você dixaria uma Inteligência Artificial decidir questões muito bem respondidas, com base científica e social, por você?",
-    alternativas:["Sim","Não"]
-},
-{//abre o objeto das perguntas
-    enunciado: "Você acha que ela vai dominar o mundo?",
-    alternativas:["Sim","Não"]
-}
+        enunciado: "Você usa ou já usou algum tipo de Inteligência Artificial?",
+            alternativas:[
+                {texto:"Sim",
+                    afirmação:"Afirmação da alternativa"},
+
+                {texto:"Não",
+                    afirmação:"Afirmação da alternativa"}]
+                },
+        {//abre o objeto das perguntas
+            enunciado: "Você acha que ela é, no minímo, relativamente boa?",
+            alternativas:[
+                {texto:"Sim",
+                    afirmação:"Afirmação da alternativa"},
+
+                {texto:"Não",
+                    afirmação:"Afirmação da alternativa"}]
+                },
+    {//abre o objeto das perguntas
+        enunciado: "Você acha que que estamos ficando mais burros e dependentes da tecnologia por influência dela?",
+        alternativas:[
+            {texto:"Sim",
+                afirmação:"Afirmação da alternativa"},
+
+            {texto:"Não",
+                afirmação:"Afirmação da alternativa"}]
+            },
+    {//abre o objeto das perguntas
+        enunciado: "Você acha que ela causará algum impacto efetivo no futuro?",
+        alternativas:[
+            {texto:"Sim",
+                afirmação:"Afirmação da alternativa"},
+
+            {texto:"Não",
+                afirmação:"Afirmação da alternativa"}]
+            },
+    {//abre o objeto das perguntas
+        enunciado: "Você dixaria uma Inteligência Artificial decidir questões muito bem respondidas, com base científica e social, por você?",
+        alternativas:[
+            {texto:"Sim",
+                afirmação:"Afirmação da alternativa"},
+
+            {texto:"Não",
+                afirmação:"Afirmação da alternativa"}]
+            },
+    {//abre o objeto das perguntas
+        enunciado: "Você acha que ela vai dominar o mundo?",
+        alternativas:[
+            {texto:"Sim",
+                afirmação:"Afirmação da alternativa"},
+
+            {texto:"Não",
+                afirmação:"Afirmação da alternativa"}]
+            },
 ]
 
 let posicao = 0;
 let perguntaAtual;
 
 function mostraPergunta(){
-    perguntaAtual = perguntas [posicao];
+    perguntaAtual = perguntas[posicao];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas();
 }
@@ -45,5 +75,4 @@ function mostraAlternativas(){
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
-
 mostraPergunta();
