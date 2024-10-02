9f6b5009c71406ac6543ee4553789fce2fc52a2f
catarinali <114243672+catarinali@users.noreply.github.com>
2024-10-02 18:28:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -71,8 +71,17 @@ function mostraPergunta(){
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmação;
+    historiaFinal = afirmacoes;
+    posicao++;
+    mostraPergunta();
+}
+
 mostraPergunta();
